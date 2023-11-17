72354115582d7ee3529e34450570700a3695129f
Mpickens3 <90661015+Mpickens3@users.noreply.github.com>
2023-11-17 11:06:38 -0800

    Update auth_page.dart

--- pages/auth_page.dart
+++ pages/auth_page.dart
@@ -1,28 +1,27 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'home_page.dart';
 import 'login_page.dart';
+import 'register_page.dart';
 
-class AuthPage extends StatelessWidget {
-  const AuthPage({super.key});
+class AuthPage extends StatefulWidget {
+   const AuthPage({Key? key}) : super(key: key);
+   @override
+   State<AuthPage> createState() => _AuthPageState();
+}
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: StreamBuilder<User?>(
-        stream: FirebaseAuth.instance.authStateChanges(),
-        builder: (context, snapshot) {
-          // user is logged in
-          if (snapshot.hasData) {
-            return HomePage();
-          }
-
-          // user is NOT logged in
-          else {
-            return LoginPage();
-          }
-        },
-      ),
-    );
-  }
+class _AuthPageState extends State<AuthPage> {
+   // initially show login page
+   bool showLoginPage = true;
+   void toggleScreens() {
+     setState(() {
+       showLoginPage = !showLoginPage;
+      });
+    } 
+   @override
+   Widget build(BuildContext context) {
+     if (showLoginPage) {
+       return LoginPage(showRegisterPage: toggleScreens); 
+       } else {
+         return RegisterPage(showLoginPage: toggleScreens);
+     }
+   }
 }
